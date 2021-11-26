ba89d61163578c4b34ac2846c6097f5349fd0ef0
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-26 19:05:22 +0530

    Update foreground.sh

--- yugabytedb/foreground.sh
+++ yugabytedb/foreground.sh
@@ -1,8 +1,7 @@
-var=0
-while [ var!=1 ]; do
-echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Environment Setup"
-var=$(docker exec -it yugabyte /home/yugabyte/bin/ysqlsh -Atc 'SELECT 1') > /dev/null 2>&1
-sleep 5
+until docker exec -it yugabyte /home/yugabyte/bin/ysqlsh -Atc 'SELECT 1'
+do
+  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Environment Setup"
+  sleep 5
 done
 docker exec -it yugabyte /bin/bash
 ysqlsh
