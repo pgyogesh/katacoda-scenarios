d016daac71018160714c738652204848303a9156
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-26 17:43:30 +0530

    Update env-setup.sh

--- yugabytedb/env-setup.sh
+++ yugabytedb/env-setup.sh
@@ -1,4 +1,4 @@
 docker pull yugabytedb/yugabyte:latest
 docker run -d --name yugabyte -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false --ui=false
-sleep 7
+sleep 10
 touch /tmp/env_created
