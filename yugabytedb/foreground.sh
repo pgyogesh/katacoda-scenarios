a860ea6bf314c62f41181a95edfef2c5033f48a4
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-26 19:10:00 +0530

    Update foreground.sh

--- yugabytedb/foreground.sh
+++ yugabytedb/foreground.sh
@@ -1,6 +1,6 @@
 until docker exec -it yugabyte /home/yugabyte/bin/ysqlsh -Atc 'SELECT 1'
 do
-  echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Environment Setup"
+  echo "Waiting for environment to setup"
   sleep 5
 done
 docker exec -it yugabyte /bin/bash
