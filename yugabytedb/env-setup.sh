53050296280bab7e32e3be3f2e12736e2bc804b4
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-26 17:40:02 +0530

    Update env-setup.sh

--- yugabytedb/env-setup.sh
+++ yugabytedb/env-setup.sh
@@ -1,5 +1,4 @@
 docker pull yugabytedb/yugabyte:latest
 docker run -d --name yugabyte -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false --ui=false
-sleep 3
-docker exec -it yugabyte /home/yugabyte/bin/ysqlsh
+sleep 7
 touch /tmp/env_created
