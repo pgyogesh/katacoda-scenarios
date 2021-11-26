9cffb09561beaccaf33ad73527ad3f12450c52b2
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-26 17:58:41 +0530

    Update foreground.sh

--- yugabytedb/foreground.sh
+++ yugabytedb/foreground.sh
@@ -1,10 +1,8 @@
-echo "checking with var"
 var=0
 until [ var=1 ]; do
 echo >&2 "$(date +%Y%m%dt%H%M%S) Waiting for Environment Setup"
-var=`ysqlsh -Atc 'SELECT 1'`
+var=$(ysqlsh -Atc 'SELECT 1')
 sleep 5
 done
-clear
 docker exec -it yugabyte /bin/bash
 ysqlsh
