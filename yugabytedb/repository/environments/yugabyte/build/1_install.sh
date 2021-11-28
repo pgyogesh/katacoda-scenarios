448b03934ec07ce4c5f75ec0c5434004f7c60d31
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-28 19:31:32 +0530

    Delete 1_install.sh

--- yugabytedb/repository/environments/yugabyte/build/1_install.sh
+++
@@ -1,3 +0,0 @@
-docker pull yugabytedb/yugabyte:latest
-docker run -d --name yugabyte  -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false --ui=false
-docker exec -it yugabyte /home/yugabyte/bin/ysqlsh
