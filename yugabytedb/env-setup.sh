f7dde3b17e93f94a49a718de6ed82417a6c8bdb6
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-28 17:36:46 +0530

    Update env-setup.sh

--- yugabytedb/env-setup.sh
+++ yugabytedb/env-setup.sh
@@ -1,6 +1,6 @@
 docker pull yugabytedb/yugabyte:latest
 echo "done" >> /root/katacoda-finished
-docker -d run --name yugabyte -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false --ui=false
+docker run -d --name yugabyte -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false --ui=false
 
 var=0
 while [ $var -eq 0 ]
