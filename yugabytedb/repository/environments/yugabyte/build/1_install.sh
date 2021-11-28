563ec2079d64b1baaf173b9103c840839ec4e630
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-28 16:52:04 +0530

    Update 1_install.sh

--- yugabytedb/repository/environments/yugabyte/build/1_install.sh
+++ yugabytedb/repository/environments/yugabyte/build/1_install.sh
@@ -1,3 +1,3 @@
-docker pull yugabytedb/yugabyte:2.11.0.0-b7
-docker run -d --name yugabyte  -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:2.11.0.0-b7 bin/yugabyted start --daemon=false --ui=false
+docker pull yugabytedb/yugabyte:latest
+docker run -d --name yugabyte  -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false --ui=false
 docker exec -it yugabyte /home/yugabyte/bin/ysqlsh
