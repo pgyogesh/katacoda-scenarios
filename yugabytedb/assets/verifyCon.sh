054e7ba66677473945f86dd4e98bc95475e8e077
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-28 18:19:39 +0530

    Update verifyCon.sh

--- yugabytedb/assets/verifyCon.sh
+++ yugabytedb/assets/verifyCon.sh
@@ -2,5 +2,6 @@ var="Not accepting"
 until [ "$var" = "accepting" ]
 do
   sleep 2
+  var=$(docker exec yugabyte /home/yugabyte/bin/ysqlsh -Atc "SELECT 'accepting'")
 done
 echo "done" >> /root/katacoda-background-finished
