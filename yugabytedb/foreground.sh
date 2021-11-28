a9cfdecf8e3228b601b6d411ae0f91000804a95d
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-28 16:53:30 +0530

    Delete foreground.sh

--- yugabytedb/foreground.sh
+++
@@ -1,8 +0,0 @@
-until docker exec -it yugabyte ls /tmp/setup.done > /dev/null 2>&1
-do
-  echo "Yugabyte is setting up. Please wait..."
-  sleep 3
-done
-clear
-docker exec -it yugabyte /bin/bash
-ysqlsh
