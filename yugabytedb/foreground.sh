ae54155485804dc6bfce3d7ef8a6faf5ad1fd1e5
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-28 13:27:32 +0530

    Update foreground.sh

--- yugabytedb/foreground.sh
+++ yugabytedb/foreground.sh
@@ -1,6 +1,6 @@
-until [ -f /tmp/setup.done ]
+until docker exec -it yugabyte /bin/bash ls /tmp/setup.done
 do
-  echo "Yugabyte is setting up"
+  echo "Yugabyte is setting up. Please wait..."
   sleep 3
 done
 clear
