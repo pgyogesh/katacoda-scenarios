7076f49b3fa322e66b425f162f0cd059cbb30636
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-28 15:06:55 +0530

    Update env-setup.sh

--- yugabytedb/env-setup.sh
+++ yugabytedb/env-setup.sh
@@ -1,4 +1,4 @@
 docker pull yugabytedb/yugabyte:latest
 echo "done" >> /root/katacoda-finished
-docker run -d --name yugabyte -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false --ui=false
+docker run --name yugabyte -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false --ui=false
 echo "done" >> /root/katacoda-background-finished
