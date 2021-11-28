16d7b0c8e4081cf915eb2b65b94a9a39aae98e17
Yogesh Jadhav <pgyogesh@outlook.com>
2021-11-28 14:50:22 +0530

    Update env-setup.sh

--- yugabytedb/env-setup.sh
+++ yugabytedb/env-setup.sh
@@ -1,6 +1,4 @@
 docker pull yugabytedb/yugabyte:latest
+echo "done" >> /root/katacoda-finished
 docker run -d --name yugabyte -p7000:7000 -p9000:9000 -p5433:5433 -p9042:9042 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false --ui=false
-until docker exec yugabyte /home/yugabyte/bin/ysqlsh -Atc 'SELECT 1' -o /tmp/setup.done > /tmp/error.log 2>&1
-do
-  sleep 1;
-done
+echo "done" >> /root/katacoda-background-finished
